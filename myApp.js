48c98bd0165d64a9f7469ab4aa3800bc8c9ae49f
Andresser17 <andresserserrano@gmail.com>
2021-12-14 13:13:40 -0400

    Connected to MongoDB Atlas

--- myApp.js
+++ myApp.js
@@ -1,5 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
+const mongoose = require("mongoose");
 
+const db = mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 let Person;
 
